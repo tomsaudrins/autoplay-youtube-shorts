1ebab21ead15d2dece5589367de01d25ac551301
Toms Audrins <67427155+tomsaudrins@users.noreply.github.com>
2022-07-15 15:06:23 +0200

    Create a chrome extension to autoplay shorts

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-videoPlayer = document.querySelector("#shorts-player > div.html5-video-container > video")
-let nextButton = document.querySelector("#navigation-button-down > ytd-button-renderer > a")
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
 
-videoPlayer.addEventListener("play", (event) => {
-    setTimeout(function(){
-        document.querySelector("#shorts-player > div.html5-video-container > video").loop = false;
-    }, 5000)
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+window.addEventListener("load", function (){
+    waitForElm("#shorts-player > div.html5-video-container > video").then((elm) => {
+        videoPlayer = document.querySelector("#shorts-player > div.html5-video-container > video")
+        let nextButton = document.querySelector("#navigation-button-down > ytd-button-renderer > a")
+    
+        videoPlayer.addEventListener("play", (event) => {
+            setTimeout(function(){
+                document.querySelector("#shorts-player > div.html5-video-container > video").loop = false;
+            }, 5000)
+        })
+    
+        videoPlayer.addEventListener("ended", () => {
+            nextButton.click()
+            document.querySelector("#shorts-player > div.html5-video-container > video").loop = false
+        })
+    
+        videoPlayer.loop = false
+    });
 })
-
-videoPlayer.addEventListener("ended", () => {
-    nextButton.click()
-    document.querySelector("#shorts-player > div.html5-video-container > video").loop = false
-})
-
-videoPlayer.loop = false
-videoPlayer.autoPlay = true
-
